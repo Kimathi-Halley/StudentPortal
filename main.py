acf632f8ad289252b1bbb459a28f7c0c5289bff6
Kimathi-Halley <kimathi.a.halley@gmail.com>
2025-05-21 16:38:22 -0400

    Added user login prompt
    Wed.21.May.25. 4:38pm
    
    Added user login feature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def welcome():
     print("Welcome to the Student Portal!")
 
+def login():
+    username = input("Enter your name: ")
+    print(f"Welcome, {username}!")
+
 if __name__ == "__main__":
     welcome()
+    login()
