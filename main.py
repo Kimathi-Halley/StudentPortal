86d1628cbd21bd59bbcbff8e62fa51ccb57c0a1e
Kimathi-Halley <kimathi.a.halley@gmail.com>
2025-05-21 16:42:56 -0400

    Added about section
    Wed.21.May.25  4:42pm
    
    Added about section of feature-about-page branch

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def login():
     username = input("Enter your name: ")
     print(f"Welcome, {username}!")
 
+def about():
+    print("About: This is a basic student portal system used for demonstrating software development and version control with Git.")
+
 if __name__ == "__main__":
     welcome()
     login()
+    about()
